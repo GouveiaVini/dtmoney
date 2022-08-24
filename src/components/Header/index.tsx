61d20644acdea543fda56f85604010b2b6f2e215
Vinicius Gouveia <gouveiavinicius96@gmail.com>
2022-08-24 17:30:49 -0300

    dtMoney

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+  onOpenNewTransaction: () => void;
+}
+
+export function Header({onOpenNewTransaction}:HeaderProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransaction}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
